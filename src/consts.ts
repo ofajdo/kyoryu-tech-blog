2ca1cd0ded6559564e366e9b92450a885fd29e1d
あああああ <kiuchiryou@icloud.com>
2025-07-05 07:57:27 +0900

    コミット

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,16 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const BLOG_CATEGORY = [
+  {
+    title: "",
+    name: "",
+  },
+];
+
+export const BLOG_TAG = [
+  {
+    title: "",
+    name: "",
+  },
+];
